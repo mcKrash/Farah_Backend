b98ee79d22a871f44a6c90cc0d2f98e5eae82b6e
Krash <krash1120@gmail.com>
2021-08-03 14:28:28 +0200

    login/signup 3/8/2021

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dataBaseUrl: 'mongodb+srv://farah-app:EyWeLL12INrC1JQb@farah.icftw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    dataBaseUrl: process.env.DATABASE_URI
     
 }
 
