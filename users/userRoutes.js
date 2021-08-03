96eecadd575911a979019db1708fbac5c0fd3e5c
Krash <krash1120@gmail.com>
2021-08-03 15:07:05 +0200

    first-attempt

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const User = require("../models/User");
+const Client = require("../models/User");
 const { check, query, body } = require("express-validator");
 
 const userControllers = require("../users/userController");
@@ -9,14 +9,14 @@ router.post(
   "/register",[
 
   check("email").custom((value) => {
-    return User.findOne({ email: value }).then((user) => {
+    return Client.findOne({ email: value }).then((user) => {
       if (user) {
         return Promise.reject("This Email Is Already In Use");
       }
     });
   }),
   check("phone").custom((value) => {
-    return User.findOne({phone : value }).then((phone) => {
+    return Client.findOne({phone : value }).then((phone) => {
       if(phone){
         return Promise.reject("This Phone Is Already In Use");
       }
@@ -24,11 +24,7 @@ router.post(
   }),
 
   body("name", "Please Enter a name").not().isEmpty(),
-  // body("email", "Please Enter a email").not().isEmpty(),
-  // body("phone", "Please Enter a Valid Phone").isLength({
-    // min: 9
-  // }),
-  body('password', 'please Enter password').isLength({min: 6})
+  body('password', 'please Enter password | Password min Length : 6  ').isLength({min: 6})
   
 ],userControllers.userSignup);
 
