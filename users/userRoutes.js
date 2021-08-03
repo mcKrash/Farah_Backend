4dbd677bd38b223ffba96c65480e79a193d65690
Krash <krash1120@gmail.com>
2021-08-03 14:42:35 +0200

    first-attempt

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -8,21 +8,23 @@ const userControllers = require("../users/userController");
 router.post(
   "/register",[
 
-  check("email").custom((value) => {
-    return User.findOne({ email: value }).then((user) => {
-      if (user) {
-        return Promise.reject("This Email Is Already In Use");
-      }
-    });
-  }),
-  check("phone").custom((value) => {
-    return User.findOne({phone : value }).then((phone) => {
-      if(phone){
-        return Promise.reject("This Phone Is Already In Use");
-      }
-    });
-  }),
+  // check("email").custom((value) => {
+  //   return User.findOne({ email: value }).then((user) => {
+  //     if (user) {
+  //       return Promise.reject("This Email Is Already In Use");
+  //     }
+  //   });
+  // }),
+  // check("phone").custom((value) => {
+  //   return User.findOne({phone : value }).then((phone) => {
+  //     if(phone){
+  //       return Promise.reject("This Phone Is Already In Use");
+  //     }
+  //   });
+  // }),
+
   body("name", "Please Enter a name").not().isEmpty(),
+  body("email", "Please Enter a email").not().isEmpty(),
   body("phone", "Please Enter a Valid Phone").isLength({
     min: 9
   }),
