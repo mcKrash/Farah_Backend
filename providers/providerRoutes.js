3bc36c106e3b3fa0a0f06c769e3faf6ef7081999
Krash <krash1120@gmail.com>
2021-08-09 15:47:46 +0200

    first-attempt

--- providers/providerRoutes.js
+++ providers/providerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Provider = require("../models/Provider");
+const Provider = require("../models/Halls");
 const { check, query, body } = require("express-validator");
 
 const providerController = require("../providers/providersController");
