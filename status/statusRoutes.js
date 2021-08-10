100ab0a29d5fca6b5f4868f7006255e00d8206c4
Krash <krash1120@gmail.com>
2021-08-10 21:25:41 +0200

    first-attempt

--- status/statusRoutes.js
+++ status/statusRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Status = require("../models/Status");
-const { check, query, body } = require("express-validator");
+
+
+
 
 const statusController = require("../status/statusController");
 
